729303c3f5c33e5a5569554149d0e4c0e3e24882
caglarcem <caglarcem@gmail.com>
2023-12-24 15:54:36 +1000

    fix api key

--- api/openapi.controller.js
+++ api/openapi.controller.js
@@ -5,14 +5,11 @@ const getOpenApiAnswer = async (req, res, next) => {
 
     console.log('Question: ', question);
 
-    // TODO Mock
-    // return res.send(
-    //     'Gatsby and Daisy had a romantic relationship that began five years ago and has continued until the present. They love each other and have been seeing each other secretly, even though Daisy is now married to Tom Buchanan.'
-    // );
-
     const answer = await getAnswerToQuestion(question);
 
     res.send(answer);
+
+    console.log('Answer: ', answer);
 };
 
 module.exports = { getOpenApiAnswer };
